96eaf8eee16444466254f24f75b7e3f94c4cf241
evanhekman <ehekman@umich.edu>
2025-11-14 14:19:28 -0500

    matrix version

--- hawkdove.py
+++ hawkdove.py
@@ -4,10 +4,10 @@ import numpy as np
 
 
 class Strategy(enum.Enum):
-    HAWK = 0
-    DOVE = 1
-    BULLY = 2
-    RETALIATOR = 3
+    HAWK = (1, 0, 0, 0)
+    DOVE = (0, 1, 0, 0)
+    BULLY = (0, 0, 1, 0)
+    RETALIATOR = (0, 0, 0, 1)
 
 
 PAYOFF = np.array(
@@ -20,64 +20,8 @@ PAYOFF = np.array(
 )
 
 
-def matrix_interaction(p1: np.ndarray, p2: np.ndarray):
-    return int(p1 @ PAYOFF @ p2), int(p2 @ PAYOFF @ p1)
-
-
-def interaction(strat1, strat2) -> tuple[int, int]:
-    match strat1:
-        case Strategy.HAWK:
-            match strat2:
-                case Strategy.HAWK:
-                    return -25, -25
-                case Strategy.DOVE:
-                    return 50, 0
-                case Strategy.BULLY:
-                    return 50, 0
-                case Strategy.RETALIATOR:
-                    return -25, -25
-                case _:
-                    raise ValueError("Invalid strategy")
-        case Strategy.DOVE:
-            match strat2:
-                case Strategy.HAWK:
-                    return 0, 50
-                case Strategy.DOVE:
-                    return 15, 15
-                case Strategy.BULLY:
-                    return 0, 50
-                case Strategy.RETALIATOR:
-                    return 15, 15
-                case _:
-                    raise ValueError("Invalid strategy")
-        case Strategy.BULLY:
-            match strat2:
-                case Strategy.HAWK:
-                    return 0, 50
-                case Strategy.DOVE:
-                    return 50, 0
-                case Strategy.BULLY:
-                    return 25, 25
-                case Strategy.RETALIATOR:
-                    return 0, 50
-                case _:
-                    ...
-        case Strategy.RETALIATOR:
-            match strat2:
-                case Strategy.HAWK:
-                    return -25, -25
-                case Strategy.DOVE:
-                    return 15, 15
-                case Strategy.BULLY:
-                    return 50, 0
-                case Strategy.RETALIATOR:
-                    return 15, 15
-                case _:
-                    ...
-        case _:
-            raise ValueError("Invalid strategy")
-    # to avoid type checker errors
-    return 0, 0
+def interaction(p1: np.ndarray, p2: np.ndarray):
+    return float(p1 @ PAYOFF @ p2), float(p2 @ PAYOFF @ p1)
 
 
 def main():
@@ -88,9 +32,15 @@ def main():
         while True:
             print("HAWK - 0, DOVE - 1, BULLY - 2, RETALIATOR - 3")
             print("player 1 select strategy: ")
-            strat1 = Strategy(int(input()))
+            index1 = int(input())
+            arr1 = [0] * 4
+            arr1[index1] = 1
+            strat1 = np.array(arr1)
             print("player 2 select strategy: ")
-            strat2 = Strategy(int(input()))
+            index2 = int(input())
+            arr2 = [0] * 4
+            arr2[index2] = 1
+            strat2 = np.array(arr2)
             r1, r2 = interaction(strat1, strat2)
             player1 += r1
             player2 += r2
@@ -100,5 +50,5 @@ def main():
         print("terminating")
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
